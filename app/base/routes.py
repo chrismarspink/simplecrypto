d12541d584e0f3f74e596d145a83c28a5ee67ff9
chrismarspink <jkkim7202@gmail.com>
2022-06-22 17:17:29 +0900

    data 2

--- app/base/routes.py
+++ app/base/routes.py
@@ -219,7 +219,7 @@ def register():
 
         #check company code in csv file
 
-        f = open('./data/company.csv', 'r', encoding='utf-8')
+        f = open('/data/company.csv', 'r', encoding='utf-8')
         rdr = csv.reader(f)
         for line in rdr:
             print(line)
